fc87b6e76b85ddbce780be3be9e462e596aa2379
unknown <enersuworks@gmail.com>
2025-03-13 18:23:19 +0800

    deleted added folder

---
+++ app.js
@@ -0,0 +1,22 @@
+var uiController = (function() {
+    var x =100;
+
+    function add(y){
+        return x + y ;
+    }
+
+    return{
+       publicAdd: function(a){
+        a = add(a);
+        console.log('bolovsruulsan utga ; ' + a);
+       } 
+    }
+})();
+
+var financeController = (function() {
+
+})();
+
+var appController = (function(uiController, financeController) {
+    uiController.publicAdd(50);
+})(uiController, financeController);
