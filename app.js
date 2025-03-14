35c57567b434e00d9e8dce16f5b9084011c0857d
unknown <enersuworks@gmail.com>
2025-03-14 16:58:39 +0800

    finance module hiij ehelsen. Expense, Income objectuudig uusgene

--- app.js
+++ app.js
@@ -1,35 +1,89 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function() {
-
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addBtn: '.add__btn'
+    }
+    return {
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 })();
 
 // Санхүүтэй ажиллах контроллер
 var financeController = (function() {
 
+var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+
+var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+
+var data = {
+    allItems: {
+        inc: [],
+        exp: []
+    },
+
+    totals: {
+        inc: 0,
+        exp: 0
+    }
+}
+
+
 })();
 
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
+
     var ctrlAddItem = function(){
-       console.log('Delgetsees ugugdluu avah heseg.'); 
-    };
-    document.querySelector('.add__btn').addEventListener('click',function(){
         // 1. Өгөгдлийг дэлгэцээс олж авна.
-        ctrlAddItem();
+        console.log(uiController.getInput());
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж хадгална.
 
         // 3. Олж авсан өгөгдлүүдийг вэб дээрээ тохирох хэсэгт гаргана.
 
         // 4. Төсвийг тооцоолно.
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+    };
+    var setupEventListeners = function(){
+        var DOM = uiController.getDOMstrings();
 
-    });
-
-    document.addEventListener("keypress", function(event){
-        if(event.keyCode === 13 || event.which === 13) {
+        document.querySelector(DOM.addBtn).addEventListener('click',function(){
             ctrlAddItem();
-        }
+        });
+    
+        document.addEventListener("keypress", function(event){
+            if(event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
 
-    });
+    return {
+        init: function(){
+            console.log('app started...');
+            setupEventListeners();
+        }
+    }
 
 })(uiController, financeController);
+
+appController.init();
