ffa91fcdff79d40d99dd0c05d76099aafc67f1b9
unknown <enersuworks@gmail.com>
2025-03-17 17:22:31 +0800

    tusviig delgetsend update hiine

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var uiController = (function() {
         inputValue: '.add__value',
         addBtn: '.add__btn',
         incomeList: '.income__list',
-        expenseList: '.expenses__list'
+        expenseList: '.expenses__list',
+        tusuvLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
     return {
         getInput: function(){
@@ -31,10 +35,18 @@ var uiController = (function() {
             });
 
             fieldsArr[0].focus();
+        },
 
-            // for (var i = 0; i < fieldsArr.length; i++) {
-            //     fieldsArr[i].value = '';
-            // }
+        tusviigUzuuleh: function(tusuv){
+            document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+            document.querySelector(DOMstrings.incomeLabel).textContent = tusuv.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = tusuv.totalExp;
+            if(tusuv.huvi !== 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi +"%";
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi;
+            }
+
         },
 
         addListItem: function(item , type){
@@ -158,7 +170,7 @@ var appController = (function(uiController, financeController) {
         var tusuv = financeController.tusviigAvah();
 
         // 6. Tootsoog delgetsend gargana.
-        console.log(tusuv);
+        uiController.tusviigUzuuleh(tusuv);
         }
 
     };
@@ -179,6 +191,12 @@ var appController = (function(uiController, financeController) {
     return {
         init: function(){
             console.log('app started...');
+            uiController.tusviigUzuuleh({
+                tusuv: 0,
+                huvi: 0,
+                totalInc: 0,
+                totalExp: 0
+            });
             setupEventListeners();
         }
     }
